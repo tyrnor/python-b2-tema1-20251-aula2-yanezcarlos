2c37fe3f3cdfc68e5dc2b47d5be9cdd3e46680c1
Carlos Yáñez <carlosyanez997@gmail.com>
2025-12-29 00:03:05 +0100

    Add solution for exercise 1 b1

--- 1b/ej1b1.py
+++ 1b/ej1b1.py
@@ -45,26 +45,27 @@ from typing import List
 
 
 def mcd(a: int, b: int) -> int:
-    while b:
+    while b != 0:
+        a, b = b, a % b
         
     return a
 
 
 def mcd_list(numbers: List[int]) -> int:
-    result = 
+    result = numbers[0]
     for number in numbers[1:]:
-        result = 
+        result = mcd(result, number)
     return result
 
 
 def mcm(a: int, b: int) -> int:
-    return 
+    return (a * b) // mcd(a, b)
 
 
 def mcm_list(numbers: List[int]) -> int:
-    result = 
+    result = numbers[0]
     for number in numbers[1:]:
-        result = 
+        result = mcm(result, number)
     return result
 
 
